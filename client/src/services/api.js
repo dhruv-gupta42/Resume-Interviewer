1ec752d0f4840df3435f4c76fa7d375ee34f2123
dhruv-gupta42 <dhruvguptaa42@gmail.com>
2026-05-22 14:54:50 +0530

    Attach auth token to API requests

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -7,4 +7,27 @@ import.meta.env.VITE_API_URL + "/api"
 
 });
 
+
+api.interceptors.request.use(
+
+(config)=>{
+
+const token=
+localStorage.getItem(
+"token"
+);
+
+if(token){
+
+config.headers.Authorization=
+`Bearer ${token}`;
+
+}
+
+return config;
+
+}
+
+);
+
 export default api;
