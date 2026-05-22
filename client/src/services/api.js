3832b3ffb8175b5550c8f761df20d2ea695e06d9
dhruv-gupta42 <dhruvguptaa42@gmail.com>
2026-05-22 14:27:34 +0530

    Prepare frontend for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,21 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({
-baseURL:"http://localhost:5000/api"
-});
+const api=axios.create({
 
-api.interceptors.request.use((config)=>{
-
-const token=localStorage.getItem("token");
-
-if(token){
-
-config.headers.Authorization=
-`Bearer ${token}`;
-
-}
-
-return config;
+baseURL:
+import.meta.env.VITE_API_URL
 
 });
 
